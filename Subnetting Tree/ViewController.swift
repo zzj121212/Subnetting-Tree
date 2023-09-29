629657c05c71175cb8ecd5fa1d591e09db4ec9da
Cliif Zhang <Zijia.Zhang@student.uts.edu.au>
2023-09-29 22:42:20 +1000

    Initial Commit

---
+++ Subnetting Tree/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Subnetting Tree
+//
+//  Created by apple on 29/9/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Subnetting TreeUITests/Subnetting_TreeUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Subnetting_TreeUITestsLaunchTests.swift
+//  Subnetting TreeUITests
+//
+//  Created by apple on 29/9/2023.
+//
+
+import XCTest
+
+final class Subnetting_TreeUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
